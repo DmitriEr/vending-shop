5d27b7a2f4fc2c2738ea866bbdf3565183251a46
Дмитрий <tomountain@mail.ru>
2020-10-11 13:04:18 +0400

    fix: correct wyas scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header';
 import { Content } from './components/Content';
 import { ModalWindow } from './components/Modal';
 import 'antd/dist/antd.css'; 
-import './App.css';
+import './App.scss';
 
 const App: React.FunctionComponent = () => (
   <div className="App">
